6789f7df0a2059daca854affee9130c11757eb35
Chris <chris@yourdreamnet.co.uk>
2014-12-29 23:04:06 +0000

    Forward to a generated URL rather than just '/'.

--- Handler/AuthenticationHandler.php
+++ Handler/AuthenticationHandler.php
@@ -28,7 +28,7 @@ implements AuthenticationSuccessHandlerInterface,
             $url = $targetPath;
         } else {
             // Otherwise, redirect him to wherever you want
-            $url = "/";
+            $url = $this->generateUrl('scout_base_app_list');
         }
         
         if ($request->isXmlHttpRequest()) {
@@ -61,7 +61,7 @@ implements AuthenticationSuccessHandlerInterface,
         } else {
             // Create a flash message with the authentication error message
             $request->getSession()->getFlashBag()->add('error', $exception->getMessage());
-            $url = "/";
+            $url = $this->generateUrl('scout_base_login');
 
             return new RedirectResponse($url);
         }
